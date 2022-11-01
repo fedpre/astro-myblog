b9362df51ab5ff242ba1d4d9bbb95b9564270fe3
fedpre <fed.pregnolato@gmail.com>
2022-11-01 14:36:21 +0100

    updated website url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://fedpre.github.io/wdd330-frontend-dev-II/',
   integrations: [sitemap(), react(), image(), tailwind()]
 })
