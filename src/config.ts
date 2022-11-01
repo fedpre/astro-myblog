066f8f2c6e2f0124da417d0785320c01947f2ff6
fedpre <fed.pregnolato@gmail.com>
2022-11-01 14:33:13 +0100

    update website info

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'WDD330 Homepage - Federico Pregnolato'
+export const SITE_DESCRIPTION =
+  'Welcome to my portal for WDD330! Please check out my notes and assignments.'
+export const HOMEPAGE_URL = 'https://www.fedpregnolato.io'
