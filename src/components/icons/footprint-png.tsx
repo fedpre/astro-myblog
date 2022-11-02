9be6d0c67312b1fa743b98a6bac4a337dbdd125a
fedpre <fed.pregnolato@gmail.com>
2022-11-02 15:50:27 +0100

    added svg logo

---
+++ src/components/icons/footprint-png.tsx
@@ -0,0 +1,9 @@
+export default function FootprintIcon() {
+  return (
+    <img
+      src="../../../public/footprint-light.png"
+      alt="Logo"
+      className="w-5 h-5 -rotate-12 dark:invert"
+    />
+  )
+}
